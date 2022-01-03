0bb518577f11f14e821616aed169734c298d9d2f
uAtomicBoolean <57876297+uAtomicBoolean@users.noreply.github.com>
2022-01-03 11:12:35 +0100

    Ajout de la lecture ligne par ligne du fichier et préparation du fichier AlgoPars.java

--- src/AlgoPars.java
+++ src/AlgoPars.java
@@ -1,10 +1,18 @@
 package AlgoPars;
 
+import AlgoPars.Metier.Programme;
+
 
 public class AlgoPars
 {
+    public AlgoPars()
+    {
+        new Programme( this, "test.algo" );
+    }
+
+
     public static void main( String[] args )
     {
-
+        new AlgoPars();
     }
 }
