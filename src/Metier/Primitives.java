595972608763227a5de37365d4bf9240f4bb4c52
logpan <logan.guesney@gmail.com>
2022-01-03 11:15:13 +0100

    Definition de Lire()

--- src/Metier/Primitives.java
+++ src/Metier/Primitives.java
@@ -2,4 +2,13 @@ package AlgoPars.Metier;
 
 public class Primitives {
 
+    public static String Lire() {
+        try (Scanner sc = new Scanner(System.in)) {
+            return sc.nextLine();
+        } catch (Exception e) {
+        }
+    }
+
+    // public static String Ecrire() {
+    // }
 }
