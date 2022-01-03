1594daa6e424741e5db50c55460231bd748ec854
Antoine <toino1503@gmail.com>
2022-01-03 11:23:52 +0100

    Ajout de Reel.java

--- src/Metier/Types/Reel.java
+++ src/Metier/Types/Reel.java
@@ -1,5 +1,16 @@
 package AlgoPars.Metier.Types;
 
 public class Reel {
+    private double valeur;
 
+    public double getValeur() {
+        return this.valeur;
+    }
+
+    public void setValeur(double v) {
+        if( this.modifiable )
+            this.valeur = v;
+        else
+            throw new RuntimeException("La valeur d'une constante n'est pas modifiable");
+    }
 }
