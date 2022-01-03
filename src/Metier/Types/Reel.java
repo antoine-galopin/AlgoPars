ee0c064b7906400a38cb1dd4dad15daeeb43f775
Jules-Latapy <76938929+Jules-Latapy@users.noreply.github.com>
2022-01-03 14:35:57 +0100

    Update Reel.java

--- src/Metier/Types/Reel.java
+++ src/Metier/Types/Reel.java
@@ -1,29 +1,9 @@
 package AlgoPars.Metier.Types;
 
-public class Reel extends Typable
+public class Reel extends Typable<Double> 
 {
-	private double valeur;
-
 	public Reel(String nom , boolean modifiable, double valeur)
 	{
-		super(nom, modifiable);
-
-		this.valeur=valeur ;
-	}
-
-	public Double getValeur() {
-		return this.valeur;
-	}
-
-	public void setValeur(double v) {
-		if( this.modifiable )
-			this.valeur = v;
-		else
-		{//si une valeur n'a pas été encore attribué
-			if(this.valeur == null)
-				this.valeur = v;
-			else
-				throw new RuntimeException("La valeur d'une constante n'est pas modifiable");
-		}
+		super(nom, modifiable, valeur);
 	}
 }
