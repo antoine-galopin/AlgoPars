d55a42bc949a01537fd051b654d75936e72e6519
uAtomicBoolean <57876297+uAtomicBoolean@users.noreply.github.com>
2022-01-03 10:52:06 +0100

    Lecture du fichier et update du compile.list

--- src/Metier/Programme.java
+++ src/Metier/Programme.java
@@ -1,6 +1,27 @@
 package AlgoPars.Metier;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 
-public class Programme {
-    
+import java.io.FileInputStream;
+
+
+public class Programme 
+{
+	private ArrayList<String> lignesFichier;
+
+
+	public Programme( String cheminFichier )
+	{
+		this.lignesFichier = new ArrayList<String>();
+
+		try
+		{
+			Scanner sc = new Scanner( new FileInputStream( cheminFichier ) );
+			
+			while ( sc.hasNextLine() )
+				this.lignesFichier.add( sc.nextLine() );
+		}
+		catch( Exception e ) { e.printStackTrace(); }
+	}
 }
