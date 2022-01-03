7453a9acace4816ef38cd868cfe2a4ab7d679d9a
logpan <logan.guesney@gmail.com>
2022-01-03 15:39:54 +0100

    Changements affichage

--- src/CUI/Affichage.java
+++ src/CUI/Affichage.java
@@ -1,8 +1,12 @@
 package AlgoPars.CUI;
 
+import AlgoPars.AlgoPars;
+
 public class Affichage {
+    private AlgoPars ctrl;
 
     public Affichage() {
+        this.ctrl = ctrl;
         System.out.println(entete());
     }
 
@@ -10,7 +14,9 @@ public class Affichage {
         String str = "¨".repeat(11);
         String sret = String.format("%-80s", str) + str + "\n";
         sret += String.format("%-80s", "|  CODE   |") + "| DONNEES |" + "\n";
-        sret += "¨".repeat(80) + " " + "¨".repeat(41);
+        sret += "¨".repeat(80) + " " + "¨".repeat(41) + "\n";
+        sret += "|" + String.format("%-80s", this.ctrl.getLigne(0)) + "|" + String.format("%5s", "NOM")
+                + String.format("%-9s", "|") + String.format("%9s", "VALEUR") + String.format("%-8s", "|");
         return sret;
     }
 
