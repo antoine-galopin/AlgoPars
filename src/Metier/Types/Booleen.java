68aa8e78b851822f06f56a8600bb4deb3e6076f9
Jules-Latapy <76938929+Jules-Latapy@users.noreply.github.com>
2022-01-03 14:39:01 +0100

    Update Booleen.java

--- src/Metier/Types/Booleen.java
+++ src/Metier/Types/Booleen.java
@@ -2,28 +2,8 @@ package AlgoPars.Metier.Types;
 
 public class Booleen extends Typable<Boolean> 
 {
-	private boolean valeur;
-
-	public Entier(String nom , boolean modifiable, boolean valeur)
+	public Booleen(String nom ,  boolean modifiable, Boolean valeur)
 	{
-		super(nom, modifiable);
-
-		this.valeur=valeur ;
-	}
-
-	public boolean getValeur() {
-		return this.valeur;
-	}
-
-	public void setValeur(boolean v) {
-		if( this.modifiable )
-			this.valeur = v;
-		else
-		{//si une valeur n'a pas été encore attribué
-			if(this.valeur == null)
-				this.valeur = v;
-			else
-				throw new RuntimeException("La valeur d'une constante n'est pas modifiable");
-		}
+		super(nom, modifiable, valeur);
 	}
 }
