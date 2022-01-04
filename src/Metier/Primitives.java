e146ddcac82625c932664c4d6fab95abac6e4207
uAtomicBoolean <57876297+uAtomicBoolean@users.noreply.github.com>
2022-01-04 09:07:55 +0100

    Ajout d'une liste d'instructions et son remplissage dans Programme.java et passage en instance de classe et non en classe static

--- src/Metier/Primitives.java
+++ src/Metier/Primitives.java
@@ -1,16 +1,31 @@
 package AlgoPars.Metier;
 
+import AlgoPars.AlgoPars;
+import AlgoPars.Metier.Programme;
+
 import java.util.Scanner;
 
-public class Primitives {
 
-    public static String Lire() {
+public class Primitives {
+    private AlgoPars ctrl;
+    private Programme prgm;
+
+    
+    public Primitives( AlgoPars ctrl, Programme prgm )
+    {
+        this.ctrl = ctrl;
+        this.prgm = prgm;
+    }
+
+   
+    public static String lire() {
         String msg = "";
         try { msg = new Scanner(System.in).nextLine(); } catch (Exception e) {}
         return msg;
     }
 
-    public static void Ecrire( AlgoPars ctrl, String msg ) {
-        return ctrl.ajouterTraceExecution(msg);
+
+    public static void ecrire( String msg ) {
+        return this.ctrl.ajouterTraceExecution(msg);
     }
 }
