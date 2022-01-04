a2c1889f0af5334cec46c0aab0fd677a2dccfd1c
logpan <logan.guesney@gmail.com>
2022-01-04 10:20:11 +0100

    Construction d'instrcution

--- src/AlgoPars.java
+++ src/AlgoPars.java
@@ -5,32 +5,31 @@ import AlgoPars.CUI.Affichage;
 
 import java.util.ArrayList;
 
-
-public class AlgoPars
-{
+public class AlgoPars {
     private Programme prgm;
     private Affichage cui;
 
-
-    public AlgoPars()
-    {
-        this.cui = new Affichage( this );
-        this.prgm = new Programme( this, "../Test/Test.algo" );
+    public AlgoPars() {
+        this.cui = new Affichage(this);
+        this.prgm = new Programme(this, "../Test/Test.algo");
         this.prgm.executerAlgo();
     }
 
-
-    public ArrayList<String> getLignesFichier() { return this.prgm.getLignesFichier(); }
-
-    public void afficher() { this.cui.afficher(); }
-    
-    public void ajouterTraceExecution( String trace ) {
-        this.cui.ajouterTraceExecution( trace );
+    public ArrayList<String> getLignesFichier() {
+        return this.prgm.getLignesFichier();
     }
 
+    public void afficher() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+        this.cui.afficher();
+    }
 
-    public static void main( String[] args )
-    {
+    public void ajouterTraceExecution(String trace) {
+        this.cui.ajouterTraceExecution(trace);
+    }
+
+    public static void main(String[] args) {
         new AlgoPars();
     }
 }
