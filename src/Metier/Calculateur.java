34eafa2cc8f867390365818ecae07a6be2dd5abe
Jules-Latapy <jullatapy@gmail.com>
2022-01-04 17:28:46 +0100

    calculateur

--- src/Metier/Calculateur.java
+++ src/Metier/Calculateur.java
@@ -5,13 +5,35 @@ public class Calculateur
 {
 	public static double calculer( String expr )
 	{
-		
-		/* if ( (index = expr.indexOf( "|" )) != -1 )
+		if (expr.isEmpty()) {
+			return 0.0 ;
+		}
+		/*
+		if ( (index = expr.indexOf( "|" )) != -1 )
 			{System.out.println(expr);
 		return calculer( expr.substring( index + 1, expr.indexOf( "|", index + 1 ) ) );} */
 
-
 		int index = 0;
+/*
+		if (expr.matches("\\|.+\\|")) {
+			index = expr.indexOf( "|" );
+
+			System.out.println(expr.substring( index + 1, expr.indexOf( "|", index + 1 ) ));
+			System.out.println(expr.substring( expr.indexOf( "|", index + 1 )));
+
+
+	return calculer(
+					String.valueOf(           calculer( expr.substring(         0,expr.indexOf( "|"))))             +
+					String.valueOf( Math.abs( calculer( expr.substring( index + 1,expr.indexOf( "|", index + 1 )))))+ 
+					String.valueOf(			  calculer( expr.substring( expr.indexOf( "|"+1))))
+					);
+		}*/
+
+		/*if (expr.matches("\w+(\w*)"))
+			primitives.find(la foncion)
+		*/
+
+
 		// Opérateurs unaires.
 		if ( (index = expr.indexOf( "+" )) != -1 )
 		{
@@ -29,7 +51,7 @@ public class Calculateur
 		}
 
 		// Opérateurs binaires.
-		if ( (index = expr.indexOf( "x" )) != -1 )
+		if ( (index = expr.indexOf( "×" )) != -1 )
 			return calculer( expr.substring( 0, index ) ) * calculer( expr.substring( index + 1, expr.length() ) );
 		if ( (index = expr.indexOf( "/" )) != -1 )
 			return calculer( expr.substring( 0, index ) ) / calculer( expr.substring( index + 1, expr.length() ) );
@@ -45,13 +67,15 @@ public class Calculateur
 
 	public static void main(String[] args) 
 	{
-		System.out.println( calculer( "5 x 4 + 3" ) + " = 23 ?" );
-		System.out.println( calculer( "5x4+3" ) + " = 23 ?" );
+		System.out.println( calculer( "5 × 4 + 3" ) + " = 23 ?" );
+		System.out.println( calculer( "5×4+3" ) + " = 23 ?" );
 		System.out.println( calculer( "+21" ) + " = 21 ?");
 		System.out.println( calculer( "-21" ) + " = -21 ?");
 		System.out.println( calculer( "20 / 8" ) + " = 2.5 ?" );
 		System.out.println( calculer( "20 div 8" ) + " = 2 ?" );
 		System.out.println( calculer( "13 mod 5" ) + " = 3 ?" );
-		System.out.println( calculer( "5 ^ 2 + 3 x 10" ) + " = 25 ?" );
+		System.out.println( calculer( "5 ^ 2 + 3 × 10" ) + " = 55 ?" );
+		System.out.println( calculer( "-5 ^ 2 - 3 × 10" ) + " = 55 ?" );
+
 	}
 }
