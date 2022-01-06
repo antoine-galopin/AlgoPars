a3498462eefdbbcc0e4f4e90b721765eb151279e
logpan <logan.guesney@gmail.com>
2022-01-06 17:26:40 +0100

    Correction des commits

--- src/Metier/Programme.java
+++ src/Metier/Programme.java
@@ -16,19 +16,14 @@ public class Programme {
 	private ArrayList<String> lignesFichierColorie;
 
 	private Donnee donnees;
-<<<<<<< HEAD
 	private ArrayList<String> listeVarSuivies;
-=======
-	private ArrayList<String> listeNomVarSuivies;
->>>>>>> 4cfb2e90f82936f2f0f2ee42f7d69dca6034b7a2
 	private ArrayList<Instruction> listeInstructions;
 
 	private boolean executionActive;
 
-
 	public Programme(AlgoPars ctrl, String cheminFichier) {
-        // Important car cela permet de charger le fichier XML des couleurs.
-        ColorationSyntaxique.chargerCouleurs();
+		// Important car cela permet de charger le fichier XML des couleurs.
+		ColorationSyntaxique.chargerCouleurs();
 
 		this.ctrl = ctrl;
 		this.primitives = new Primitives(this.ctrl, this);
@@ -39,48 +34,52 @@ public class Programme {
 		this.executionActive = true;
 
 		this.donnees = new Donnee();
-<<<<<<< HEAD
 		this.listeVarSuivies = new ArrayList<String>();
-=======
-		this.listeNomVarSuivies = new ArrayList<String>();
->>>>>>> 4cfb2e90f82936f2f0f2ee42f7d69dca6034b7a2
 		this.listeInstructions = new ArrayList<Instruction>();
 
 		try {
 			// Lecture du programme.
-			Scanner sc = new Scanner(new FileInputStream(cheminFichier), "UTF-8" );
+			Scanner sc = new Scanner(new FileInputStream(cheminFichier), "UTF-8");
 
 			String ligne = "";
-			while(sc.hasNextLine()) {
+			while (sc.hasNextLine()) {
 				ligne = sc.nextLine().replace("\t", "    ");
 				this.lignesFichier.add(ligne);
-				this.lignesFichierColorie.add( ColorationSyntaxique.colorierLigne(ligne) );
+				this.lignesFichierColorie.add(ColorationSyntaxique.colorierLigne(ligne));
 				this.listeInstructions.add(new Instruction(this.ctrl, this.primitives, ligne));
 			}
 
 			sc.close();
 
 			// Lecture du fichier contenant les variables à suivre.
-			sc = new Scanner( new FileInputStream( "../utilisateur/variables.var" ), "UTF-8" );
+			sc = new Scanner(new FileInputStream("../utilisateur/variables.var"), "UTF-8");
 
-			while ( sc.hasNextLine() )
-				this.listeVarSuivies.add( sc.next().strip() );
+			while (sc.hasNextLine())
+				this.listeVarSuivies.add(sc.next().strip());
 
-	
-		} catch( Exception e ) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
+	public int getLigneActive() {
+		return this.ligneActive;
+	}
 
-	public int getLigneActive() { return this.ligneActive; }
-	public ArrayList<String> getLignesFichier() { return this.lignesFichier; }
-	public ArrayList<String> getLignesFichierColorie() { return this.lignesFichierColorie; }
-	
-	public ArrayList<String> getVariablesSuivies() { return this.listeVarSuivies; }
+	public ArrayList<String> getLignesFichier() {
+		return this.lignesFichier;
+	}
+
+	public ArrayList<String> getLignesFichierColorie() {
+		return this.lignesFichierColorie;
+	}
+
+	public ArrayList<String> getVariablesSuivies() {
+		return this.listeVarSuivies;
+	}
 
 	public String getValeur(String nom) {
-		if( this.donnees.rechercheParNom(nom) != null )
+		if (this.donnees.rechercheParNom(nom) != null)
 			return this.donnees.rechercheParNom(nom).getValeur().toString();
 		return null;
 	}
@@ -93,35 +92,37 @@ public class Programme {
 	 * Exécution de l'algorithme.
 	 */
 	public void executerAlgo() {
-		do 
-		{
+		do {
 			this.listeInstructions.get(this.ligneActive).interpreterLigne();
 			this.ctrl.afficher();
-			
+
 			try {
 				Scanner sc = new Scanner(System.in); // ouverture du Scanne
-				
+
 				String msg = sc.nextLine();
 
-				switch(msg) {
+				switch (msg) {
 					case "b": {
-						if( this.ligneActive != 0 )
+						if (this.ligneActive != 0)
 							--this.ligneActive; // on recule d'une ligne si possible
 						break;
 					}
-					case "" : ++this.ligneActive; // on avance d'une ligne
+					case "":
+						++this.ligneActive; // on avance d'une ligne
 				}
-				
-				//sc.close(); // fermeture du Scanner
+
+				// sc.close(); // fermeture du Scanner
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-		}while (this.executionActive && this.ligneActive < this.lignesFichier.size() );
+		} while (this.executionActive && this.ligneActive < this.lignesFichier.size());
 	}
 
+	public void add(String nom, String type, String valeur) {
+		this.donnees.add(nom, type, valeur);
+	}
 
-	public void add(String nom, String type, String valeur) { this.donnees.add(nom, type, valeur); }
-
-
-    public void add(String nom, String type) { this.donnees.add(nom, type); }
+	public void add(String nom, String type) {
+		this.donnees.add(nom, type);
+	}
 }
